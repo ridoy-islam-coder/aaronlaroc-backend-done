1aab1d90fda0ea354fc80a05a6189dd68d06a6af
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2025-12-25 03:20:31 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,26 @@ import { SubscriptionRoutes } from './app/modules/subscriptions-information/subs
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:5173","http://localhost:5174" ], credentials: true,}));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://aaronlaroc-frontend.vercel.app"
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true); // allow non-browser requests
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
+// app.use(cors({ origin: ["http://localhost:5173","http://localhost:5174","https://aaronlaroc-frontend.vercel.app"], credentials: true,}));
 // app.use(cors());
 app.use(express.json({  limit: '50mb'}));
 app.use(helmet());
